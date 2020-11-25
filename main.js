4ed59d9dbbac5d1d77400da3b1f24e5491fc8db4
Yuri Sanguino Rodriguez <=>
2020-11-24 20:42:01 -0500

    operadores2

---
+++ main.js
@@ -0,0 +1,59 @@
+/*let numero_1 =5;
+let numero_2=10;
+//Incremento
+numero_1++; //Incrementar el número '1';
+numero_1 +=2 //Incrementar el número la cantidad necesaria;
+console.log(numero_1);*/
+
+//Decremento
+/*
+let numero_1 =5;
+let numero_2=10;
+numero_1--;
+numero_1 -=2 //Decrementa el número la cantidad necesaria;
+console.log(numero_1);*/
+
+//Exponenciación
+/*
+let numero_1 =5;
+let numero_2=10;
+numero_1 **=2;
+console.log(numero_1);*/
+
+
+//Operaciones de módulo según el valor
+/*
+let numero_1 =14;
+let numero_2=3;
+numero_1 %=numero_2;
+console.log(numero_1);*/
+
+
+//Operaciones matemáticas simples
+/*let numero_1 =5;
+let numero_2=2;
+let resultado;
+
+resultado = numero_1+numero_2;
+console.log(resultado);
+resultado = numero_1-numero_2;
+console.log(resultado);
+resultado = numero_1*numero_2;
+console.log(resultado);
+resultado = numero_1/numero_2;
+console.log(resultado);
+resultado = numero_1%numero_2;
+console.log(resultado);
+resultado = numero_1**numero_2;
+console.log(resultado);
+resultado = Math.sqrt(numero_1); //Raíz cuadrada
+console.log(resultado);
+resultado = Math.cbrt(numero_1); //Raíz cúbica
+console.log(resultado);*/
+
+
+
+
+
+
+
